5148b74363be72c9bd5ded326635a04fa9db874a
Nuno Martins <nmartins@redhat.com>
2026-03-09 13:56:57 +0200

    Create setup-node01.sh

---
+++ setup-automation/setup-node01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+systemctl stop systemd-tmpfiles-setup.service
+systemctl disable systemd-tmpfiles-setup.service
+
+nmcli connection add type ethernet con-name eth1 ifname eth1 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
+nmcli connection up eth1
+echo "192.168.1.10 control.lab control" >> /etc/hosts
+
+rm -rf /etc/yum.repos.d/*
+yum clean all
+subcription-manager clean
+
+curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+update-ca-trust
+rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
+subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
+
+##
+########
+## install python3 libraries needed for the Cloud Report
+dnf install -y python3-pip python3-libsemanage
+
