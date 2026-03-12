a3e2883264c48b56c6caf31d6106842100b91693
Nuno Martins <nmartins@redhat.com>
2026-03-12 13:19:36 +0200

    Update setup-central.sh

--- setup-automation/setup-central.sh
+++ setup-automation/setup-central.sh
@@ -9,8 +9,8 @@ echo "192.168.1.12 vault.zta.lab vault" >> /etc/hosts
 echo "192.168.1.13 wazuh.zta.lab wazuh" >> /etc/hosts
 echo "192.168.1.14 node01.zta.lab node01" >> /etc/hosts
 
-nmcli connection add type ethernet con-name eth1 ifname eth1 ipv4.addresses 192.168.1.11/24 ipv4.method manual connection.autoconnect yes
-nmcli connection up eth1
+nmcli connection add type ethernet con-name enp2s0 ifname eth1 ipv4.addresses 192.168.1.11/24 ipv4.method manual connection.autoconnect yes
+nmcli connection up enp2s0
 
 
 rm -rf /etc/yum.repos.d/*
